7e9ce52a6eab112ff337a4a7d9a8084ec218caf0
Tim Jones <tdjones74021@yahoo.com>
2018-04-27 15:43:41 -0500

    Initial commit.

---
+++ src/main-page/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header';
+import FeaturedHouse from './featured-house';
+
+class App extends Component {
+  // Initialize the state object of the component.
+  state = {};
+
+  componentDidMount() {
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+    });
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+        <FeaturedHouse house={this.state.featuredHouse} />
+      </div>
+    );
+  }
+}
+
+export default App;
